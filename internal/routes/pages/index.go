b0294601b230f28a28191676929108eaba85fe12
Marco Antônio <ma.souza.junior@gmail.com>
2023-10-09 10:58:23 -0300

    fea: generate mailto link on the server

--- internal/routes/pages/index.go
+++ internal/routes/pages/index.go
@@ -1,9 +1,41 @@
 package pages
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"net/url"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type IndexProps struct {
+	Params
+	PrimaryBtn   string
+	SecondaryBtn string
+}
+
+func contactURL() string {
+	q := url.Values{}
+	q.Add("subject", "Hi Marco, Let's have a coffee")
+
+	// mailto does not work with spaces as '+'
+	contact := "mailto:marco@tremtec.com?" + strings.ReplaceAll(
+		q.Encode(), "+", "%20",
+	)
+
+	log.Println("Contact Link generated", contact)
+
+	return contact
+}
 
 func rootHandler(c *fiber.Ctx) error {
-	return c.Render("index", defaultParams)
+	props := IndexProps{
+		PrimaryBtn:   contactURL(),
+		SecondaryBtn: "/resume",
+		Params:       defaultParams,
+	}
+
+	return c.Render("index", props)
 }
 
 func root(router fiber.Router) {
